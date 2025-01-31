7c138ab40cf0ad4adcfd9b72d34d438eef70aafd
Gyovani <gyovaniyuri10@hotmail.com>
2025-01-31 01:26:10 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.hosts << "fasc-ruby-production.up.railway.app"
-  config.hosts << "fasc-ruby-production.up.railway.app"
+  config.hosts.clear
 
   config.action_mailer.perform_caching = false
 
